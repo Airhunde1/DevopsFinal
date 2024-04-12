79eaeea5ff72f1b367925653f91858b7ad5e69fd
Mary Oghogho Airhun'de <42832876+Airhunde1@users.noreply.github.com>
2024-04-12 13:39:12 -0400

    Update myapp3.py

--- myapp3/myapp3.py
+++ myapp3/myapp3.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
- 
+
+# Define a route to display company name, developer's name, and student ID
 @app.route('/')
-def hello_cloud():
-  return 'Hello from Airhunde ECS Container'
- 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+def index():
+    company_name = "Wild Rydes"
+    developer_name = "Mary Airhunde"
+    student_id = "100953904"
+    return f"Company Name: {company_name}<br>Developer: {developer_name}<br>Student ID: {student_id}"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
